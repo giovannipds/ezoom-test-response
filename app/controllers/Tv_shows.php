716c46017c9dde33f25ae17964935826909abb7b
Giovanni Pires da Silva <giovannipds@gmail.com>
2018-02-14 09:16:16 -0200

    Rename TV Shows references to prevent CI errors

--- app/controllers/Tv_shows.php
+++ app/controllers/Tv_shows.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TV_Shows extends CI_Controller {
+class Tv_shows extends CI_Controller {
 
 	public function __construct()
 	{
--- app/models/Tv_shows_model.php
+++ app/models/Tv_shows_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TV_Shows_model extends CI_Model {
+class Tv_shows_model extends CI_Model {
 
     public $table = 'tv_shows';
 
