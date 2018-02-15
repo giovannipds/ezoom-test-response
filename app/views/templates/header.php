c1fdfbf4ec0aaedb52f2a84faf1eaf33fcd65d7a
Giovanni Pires da Silva <giovannipds@gmail.com>
2018-02-15 09:38:24 -0200

    Add viewport + ie compatible tags

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,6 +4,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Teste Ezoom</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<link rel="stylesheet" href="https://bootswatch.com/4/cyborg/bootstrap.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		<style type="text/css">
